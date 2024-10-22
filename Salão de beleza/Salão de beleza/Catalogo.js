5e0ae2d1bfe67d10c2174a6a0d1781405fb69e24
Viniciusss1 <150733111+Viniciusss1@users.noreply.github.com>
2024-10-22 17:23:29 -0300

    Add files via upload

---
+++ Salão de beleza/Salão de beleza/Catalogo.js
@@ -0,0 +1,17 @@
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
